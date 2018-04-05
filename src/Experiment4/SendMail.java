32f8d9db7eabf8c3e013f4ce638c15b40e479786
Mohtasin Meherab Sikder <30768212+Rusho-RU@users.noreply.github.com>
2018-04-06 00:04:20 +0600

    6th April, 2018

--- src/Experiment4/SendMail.java
+++ src/Experiment4/SendMail.java
@@ -1,10 +1,40 @@
 package Experiment4;
 
-public class SendMail {
-    private String SMTPServerIp;
-    private String port;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+class SendMail{
     private String sender;
     private String recipient;
-    private String message;
+    private String host;
 
+    SendMail(){
+        sender = "admin@localhost";
+        recipient = "dragontwister@localhost";
+        host = "localhost";
+    }
+
+    public void send(String message){
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            MimeMessage msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress(sender));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            msg.setSubject("Test Subject");
+            msg.setText(message);
+
+            Transport.send(msg);
+            System.out.println("Success!!");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
 }
---
+++ src/Experiment4/TestSendMail.java
@@ -0,0 +1,8 @@
+package Experiment4;
+
+public class TestSendMail {
+    public static void main(String[] args){
+        SendMail sendMail = new SendMail();
+        sendMail.send("Hello!");
+    }
+}
